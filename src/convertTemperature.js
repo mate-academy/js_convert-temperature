8d265bed501eb1c2b91d3a4c9745fe2e1832bed5
JukaChu <olexiy.yankevich@gmail.com>
2019-05-14 20:07:08 +0300

    fe_mar19_1_oleksii

--- src/convertTemperature.js
+++ src/convertTemperature.js
@@ -22,7 +22,36 @@
  * @return {object} - converted temperatures
  */
 function convertTemperature(temperature) {
-  // write code here
+  const tempObject = {};
+  const typeOfTemp = temperature.charAt(temperature.length - 1);
+  let ourTemperature = temperature.slice(0, -1);
+  if (typeOfTemp !== 'C' && typeOfTemp !== 'K' && typeOfTemp !== 'F') {
+    return null;
+  }
+  for (let i = 1; i < ourTemperature.length; i++) {
+    if (ourTemperature[i] < '\u0030' || ourTemperature[i] > '\u0039') {
+      return null;
+    }
+  }
+  ourTemperature = +ourTemperature;
+  if (typeOfTemp === 'C') {
+    tempObject['C'] = Math.round(ourTemperature);
+    tempObject['F'] = Math.round((ourTemperature * 9 / 5) + 32);
+    tempObject['K'] = Math.round((ourTemperature + 273.15));
+    return tempObject;
+  } else if (typeOfTemp === 'K') {
+    tempObject['K'] = Math.round(ourTemperature);
+    tempObject['C'] = Math.round(ourTemperature - 273.15);
+    tempObject['F'] = Math.round((ourTemperature - 273.15) * 9 / 5 + 32);
+    return tempObject;
+  } else if (typeOfTemp === 'F') {
+    tempObject['F'] = Math.round(ourTemperature);
+    tempObject['K'] = Math.round((ourTemperature - 32) * 5 / 9 + 273.15);
+    tempObject['C'] = Math.round((ourTemperature - 32) * 5 / 9);
+    return tempObject;
+  } else {
+    return null;
+  }
 }
 
 module.exports = convertTemperature;
